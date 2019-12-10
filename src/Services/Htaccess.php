7f02a1d63bca46ab5f098d596987bf140d543fa9
Gmulti <thomasdeneulin@gmail.com>
2019-12-10 18:57:32 +0100

    Prevent infinite redirection on image

--- src/Services/Htaccess.php
+++ src/Services/Htaccess.php
@@ -6,32 +6,32 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
+use ImageSeoWP\Helpers\CleanUrl;
+
 class Htaccess
 {
-
     const INSERT_REGEX = '@\n?# Created by ImageSEO(?:.*?)# End of ImageSEO\n?@sm';
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->filename = ABSPATH . '/.htaccess';
     }
 
     /**
-     *
-     * @return boolean
+     * @return bool
      */
-    public function isWritable(){
+    public function isWritable()
+    {
         return is_writable($this->filename);
     }
-    
+
     /**
-     * Generate content for redirection htaccess
-     *
-     * @return void
+     * Generate content for redirection htaccess.
      */
     public function generate()
     {
         $data = get_transient('_imageseo_redirect_images');
-        if(empty($data) || $data === false){
+        if (empty($data) || false === $data) {
             return '';
         }
 
@@ -44,12 +44,17 @@ class Htaccess
         $now = time();
         $delay = WEEK_IN_SECONDS * 2;
         foreach ($data as $key => $value) {
-            if(!isset($value['date_add']) || $now - $value['date_add'] > $delay){
+            if (!isset($value['date_add']) || $now - $value['date_add'] > $delay) {
+                continue;
+            }
+
+            $targetExist = CleanUrl::removeScheme(CleanUrl::removeDomainFromFilename($value['target']));
+            if (array_key_exists($targetExist, $text)) {
                 continue;
             }
 
             // /wp-content/uploads/2019/12/example.jpg http://example.local/wp-content/uploads/2019/12/new-image.jpg'
-            $text[] = sprintf('RedirectPermanent %s %s', $key, $value['target']);
+            $text[$key] = sprintf('RedirectPermanent %s %s', $key, $value['target']);
         }
 
         // End of mod_rewrite
@@ -65,49 +70,51 @@ class Htaccess
     }
 
     /**
-     *
-     * @param boolean $existing
+     * @param bool   $existing
      * @param string $newContent
+     *
      * @return string
      */
-    public function get( $existing = false, $newContent ) {
-		if ( $existing ) {
-			if ( preg_match( self::INSERT_REGEX, $existing ) > 0 ) {
-				$newContent = preg_replace( self::INSERT_REGEX, str_replace( '$', '\\$', $newContent ), $existing );
-			} else {
-				$newContent = $newContent . "\n" . trim( $existing );
-			}
-		}
+    public function get($existing = false, $newContent)
+    {
+        if ($existing) {
+            if (preg_match(self::INSERT_REGEX, $existing) > 0) {
+                $newContent = preg_replace(self::INSERT_REGEX, str_replace('$', '\\$', $newContent), $existing);
+            } else {
+                $newContent = $newContent . "\n" . trim($existing);
+            }
+        }
 
-		return trim( $newContent );
-	}
-
+        return trim($newContent);
+    }
 
     /**
-     * Save content in htaccess
+     * Save content in htaccess.
      *
      * @param string $content
-     * @return boolean
+     *
+     * @return bool
      */
-    public function save($content){
+    public function save($content)
+    {
         $existing = false;
 
         try {
-            if ( file_exists( $this->filename ) ) {
-                $existing = file_get_contents( $this->filename );
-            } 
+            if (file_exists($this->filename)) {
+                $existing = file_get_contents($this->filename);
+            }
 
-            $file = @fopen( $this->filename, 'w' );
-            if ( $file ) {
-                $result = fwrite( $file, $this->get( $existing, $content ) );
-                fclose( $file );
-    
-                return $result !== false;
+            $file = @fopen($this->filename, 'w');
+            if ($file) {
+                $result = fwrite($file, $this->get($existing, $content));
+                fclose($file);
+
+                return false !== $result;
             }
         } catch (\Exception $e) {
             return false;
         }
-    
-		return false;
+
+        return false;
     }
 }
