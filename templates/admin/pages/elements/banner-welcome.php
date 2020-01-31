3ba881baefb4188e3290cf77fb97ea725029075f
Gmulti <thomasdeneulin@gmail.com>
2020-01-31 10:21:17 +0100

    Update text welcome board

--- templates/admin/pages/elements/banner-welcome.php
+++ templates/admin/pages/elements/banner-welcome.php
@@ -9,5 +9,6 @@ if (!defined('ABSPATH')) {
     <img class="imageseo-block__image-introduce" src="<?php echo IMAGESEO_URL_DIST . '/images/settings-banner.png'; ?>" />
     <img class="imageseo-block__image-oval" src="<?php echo IMAGESEO_URL_DIST . '/images/oval.png'; ?>" />
     <h2><?php _e('Welcome on board!', 'imageseo'); ?></h2>
-    <p><?php _e('You are one step away to optimize your images for SEO and Accessibility using Artificial Intelligence!', 'imageseo'); ?></p>
+    <p style="margin-bottom:0;"><?php _e('You are one step away to optimize your images for SEO, Accessibility and Social Media sharing!', 'imageseo'); ?></p>
+    <p><?php _e("You need an account to activate the plugin. It's free!", 'imageseo'); ?></p>
 </div>
