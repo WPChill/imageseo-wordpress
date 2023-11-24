d1850d77becc3264367540bf8df5b3160d323e5a
Razvan Aldea <44024114+razvanaldea89@users.noreply.github.com>
2023-11-24 14:58:54 +0200

    no more mail

--- src/Actions/Admin/Ajax/Register.php
+++ src/Actions/Admin/Ajax/Register.php
@@ -52,12 +52,7 @@ class Register
                 'code' => 'unknown_error',
             ]);
         }
-		// Create the email subject and body
-	    $subject = __( 'Welcome to Image SEO', 'imageseo' );
-	    $body    = __( 'Welcome to Image SEO', 'imageseo' ) . "\n\n" . __( 'You can now log in to your account with the following credentials:', 'imageseo' ) . "\n\n" . __( 'Email:', 'imageseo' ) . ' ' . $email . "\n" . __( 'Password:', 'imageseo' ) . "\n\n";
-		// Email the client with the credentials
-	    wp_mail( $email, $subject, $body );
-
+		
         wp_send_json_success([
             'user' => $newUser,
         ]);
