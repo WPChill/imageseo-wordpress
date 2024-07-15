7ed74c60d15600c84a0f41ceeab85f8d75f20298
Cosmin Cristea <cristea.cosmin87@gmail.com>
2024-07-15 14:37:59 +0300

    Script minification

--- dist/adminv2/index.asset.php
+++ dist/adminv2/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9770ee0bcdd9f55be14e');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2eed19bf6cfb92ed1b2b');
--- dist/settingsv2/index.asset.php
+++ dist/settingsv2/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c15c473d467a705c9f0a');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a50f3b149e9a08c3f91b');
