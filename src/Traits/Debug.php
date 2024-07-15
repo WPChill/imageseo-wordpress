9c76df95ba3e03302428bdefc20802e9e0afec92
Cosmin Cristea <cristea.cosmin87@gmail.com>
2024-07-15 14:38:49 +0300

    Vulnerability fix

--- src/Traits/Debug.php
+++ src/Traits/Debug.php
@@ -2,27 +2,39 @@
 
 namespace ImageSeoWP\Traits;
 
-trait Debug
-{
-	public function resetDebug()
-	{
-		update_option($this->debugOption, []);
+trait Debug {
+
+	private $debugLock = 'imageseo_bulk_optimizer_debug_lock';
+	public function resetDebug() {
+		update_option( $this->debugOption, array() );
 	}
 
-	public function getDebug()
-	{
-		return get_option($this->debugOption, []);
+	public function getDebug() {
+		return get_option( $this->debugOption, array() );
 	}
 
-	public function writeDebug($value)
-	{
-		$currentDebug = get_option($this->debugOption, []);
-		if ($this->debug) {
-			if (is_array($value))
-				update_option($this->debugOption, array_merge(
-					$currentDebug,
-					is_array($value) ? $value : [$value]
-				));
+	public function writeDebug( $value ) {
+		while ( get_transient( $this->debugLock ) ) {
+			usleep( 1000 );
 		}
+		set_transient( $this->debugLock, true, 10 );
+
+		$currentDebug = $this->getDebug();
+		if ( $this->debug ) {
+			if ( is_array( $value ) ) {
+				update_option(
+					$this->debugOption,
+					array_merge(
+						$currentDebug,
+						$value
+					)
+				);
+			} else {
+				$currentDebug[] = $value;
+				update_option( $this->debugOption, $currentDebug );
+			}
+		}
+
+		delete_transient( $this->debugLock );
 	}
 }
